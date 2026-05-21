11869b70e84514eb064b6f66243faf4ad8c3e4ef
Tewodiros Fikadu <tewodrosfikadu499@gmail.com>
2026-05-21 18:28:05 +0300

    your message

--- addons/messob_fleet/models/__init__.py
+++ addons/messob_fleet/models/__init__.py
@@ -2,13 +2,17 @@
 # ---------------------------------------------------------------------------
 # MESSOB Fleet Management System - Models Package
 # ---------------------------------------------------------------------------
+# Import audit_log FIRST because it contains the base.model.audit.mixin
+# that other models inherit from
+from . import audit_log
+
+# Now import models that use the audit mixin
 from . import trip_request
 from . import trip_request_dispatch
 from . import trip_driver
 from . import fuel_log
 from . import maintenance_log
 from . import maintenance_alert
-from . import audit_log
 from . import fms_driver
 from . import location
 from . import gps_device
